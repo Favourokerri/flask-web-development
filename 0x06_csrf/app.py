1f0203671e3c95daa0a2c7cbaf158ae77e8978e0
Favour okerri <favourokerri767@gmail.com>
2023-08-23 17:06:46 -0700

    crsf

--- 0x06_csrf/app.py
+++ 0x06_csrf/app.py
@@ -5,3 +5,5 @@ app.config['SECRET_KEY'] = 'hola-you will never guess me ^&%$ldk890923'
 
 #the above is how to set a secret key to protect
 #against cross site request forgery
+# in the future we will store this secret key in an 
+# environmental variable instaed of just passing it this way
