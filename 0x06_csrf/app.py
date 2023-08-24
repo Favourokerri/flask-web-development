b44696e487af5ba008e669e15a73e7148e2891a1
Favourokerri <111712988+Favourokerri@users.noreply.github.com>
2023-08-23 17:03:17 -0700

    Update app.py

--- 0x06_csrf/app.py
+++ 0x06_csrf/app.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hola-you will never guess me ^&%$ldk890923'
 
 #the above is how to set a secret key to protect
-#to protect against cross site request forgery
+#against cross site request forgery
